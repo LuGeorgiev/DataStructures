100257ac232bb789ac44ab8065ec3d29b44bf21b
Lyubomir Georgiev <LuGeorgiev@globul.bg>
2019-02-11 18:19:55 +0200

    combinig exvercise

---
+++ CombineDSexcercise/ShoppingCenter/ProductsRepository.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace ShoppingCenter
+{
+    public class ProductsRepository
+    {
+        private Dictionary<string,OrderedBag< Product>> byProducer;
+        private Dictionary<string,OrderedBag< Product>> byName;
+        private OrderedBag<Product> byPrice;
+
+        public ProductsRepository()
+        {
+            this.byProducer = new Dictionary<string, OrderedBag<Product>>();
+            this.byName = new Dictionary<string, OrderedBag<Product>>();
+            this.byPrice = new OrderedBag<Product>((x,y)=>x.Price.CompareTo(y.Price));
+            //this.byPrice = byPrice = new OrderedBag<Product>(
+            //    (x,y)=>x.Price.CompareTo(y.Price)
+            //    );
+        }
+
+        public void Add(Product product)
+        {
+            if (!this.byProducer.ContainsKey(product.Producer))
+            {
+                this.byProducer[product.Producer] = new OrderedBag<Product>();
+            }
+            this.byProducer[product.Producer].Add( product);
+
+            if (!this.byName.ContainsKey(product.Name))
+            {
+                this.byProducer[product.Name] = new OrderedBag<Product>();
+            }
+            this.byName[product.Name].Add( product);
+
+
+            this.byPrice.Add(product);
+        }
+
+        public int DeleteProducts(string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                return 0;
+            }
+            IEnumerable<Product> productsToRemove = this.byProducer[producer];
+            int count = 0;
+            foreach (var product in productsToRemove)
+            {
+                var name = product.Name;
+                this.byName[name].Remove(product);
+                this.byPrice.Remove(product);
+                count++;
+            }
+
+            this.byProducer.Remove(producer);
+            return count;
+        }
+
+        public int DeleteProducts(string productName, string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                return 0;
+            }
+            IEnumerable<Product> productsToRemove = this.byProducer[producer]
+                .Where(x => x.Name == productName)
+                .ToList();
+            int count = 0;
+            foreach (var product in productsToRemove)
+            {
+                var name = product.Name;
+                this.byName[name].Remove(product);
+                this.byPrice.Remove(product);
+                count++;
+            }
+            if (count != 0)
+            {
+                this.byProducer.Remove(producer);
+            }
+
+            return count;
+        }
+
+        public IEnumerable<Product> FindProductsByName(string name)
+        {
+            if (!this.byName.ContainsKey(name))
+            {
+                Console.WriteLine("No products found");
+                return Enumerable.Empty<Product>();
+
+            }
+            return this.byName[name];
+        }
+
+        public IEnumerable<Product> FindProductsByProducer(string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                Console.WriteLine("No products found");
+                return Enumerable.Empty<Product>();
+            }
+            return this.byProducer[producer];
+        }
+
+        public IEnumerable<Product> FindProductsByPriceRange(double low, double high)
+        {
+            //return this.byPrice.Where(x=>x.Price>=low && x.Price<=high);
+
+            return this.byPrice
+                .Range(new Product("", low, ""), true, new Product("", high, ""), true)
+                .OrderBy(x => x);
+        }
+
+    }
+}
---
+++ CombineDSexcercise/ShoppingCenter/StartUp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenter
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var shoppingCenter = new ProductsRepository();
+            int commandNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandNumber; i++)
+            {
+                string line = Console.ReadLine();
+                int firstSpace = line.IndexOf(' ');
+                string command = line.Substring(0, firstSpace);
+                string[] tokens = line
+                    .Substring(firstSpace + 1)
+                    .Split(';');
+
+                if (command == "AddProduct")
+                {
+                    var name = tokens[0];
+                    var price = double.Parse(tokens[1]);
+                    var producer = tokens[2];
+                    var product = new Product(name, price, producer);
+
+                    shoppingCenter.Add(product);
+                }
+                else if (command == "DeleteProducts")
+                {
+                    if (tokens.Length == 1)
+                    {
+                        Console.WriteLine(shoppingCenter.DeleteProducts(tokens[0]));
+                    }
+                    else
+                    {
+                        Console.WriteLine(shoppingCenter.DeleteProducts(tokens[0],tokens[1]));
+
+                    }
+                }
+                else if (command == "FindProductsByName")
+                {
+                    var result = shoppingCenter.FindProductsByName(tokens[0]).ToList();
+                    if (result.Count!=0)
+                    {
+                        result.ForEach(x=>Console.WriteLine(x));
+                    }
+                    else
+                    {
+                        Console.WriteLine("No products found!");
+                    }
+                }
+                else if (command == "FindProductsByProducer")
+                {
+                    var result = shoppingCenter.FindProductsByProducer(tokens[0]).ToList();
+                    if (result.Count != 0)
+                    {
+                        result.ForEach(x => Console.WriteLine(x));
+                    }
+                    else
+                    {
+                        Console.WriteLine("No products found!");
+                    }
+                }
+                else if (command == "FindProductsByPriceRange")
+                {
+                    var result = shoppingCenter
+                        .FindProductsByPriceRange(
+                             double.Parse(tokens[0]),
+                             double.Parse(tokens[1])
+                         )
+                         .OrderBy(x=>x)
+                         .ToList();
+                    if (result.Count != 0)
+                    {
+                        result.ForEach(x => Console.WriteLine(x));
+                    }
+                    else
+                    {
+                        Console.WriteLine("No products found!");
+                    }
+                }
+
+            }
+        }
+    }
+}

dd0b4864c1b1afc130de26d962a1b1781f34681a
Lyubomir Georgiev <LuGeorgiev@globul.bg>
2019-02-12 16:22:49 +0200

    Invaders 80/100

---
+++ Exam09092017/01.Invaders/Invaders/Computer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Computer : IComputer
+{
+    
+    private OrderedBag<IInvader> invaders;
+    private int energy;
+
+    public Computer(int energy)
+    {
+        if (energy <= 0)
+        {
+            throw new ArgumentException();
+        }
+        this.Energy = energy;
+        this.invaders = new OrderedBag<IInvader>();
+    }
+
+    public int Energy
+    {
+        private set
+        {            
+            this.energy = value;
+        }
+        get
+        {
+            return this.energy < 0 
+                ? 0 
+                : this.energy;
+        }
+    }
+
+    public void Skip(int turns)
+    {
+        if (turns<=0)
+        {
+            return;
+        }
+
+        foreach (var invader in this.invaders)
+        {
+            invader.Distance -= turns;
+            if (invader.Distance<=0)
+            {
+                this.energy -= invader.Damage;
+            }
+        }
+
+        this.invaders.RemoveAll(x => x.Distance <= 0);
+    }
+
+    public void AddInvader(Invader invader)
+    {
+        this.invaders.Add(invader);
+    }
+
+    public void DestroyHighestPriorityTargets(int count)
+    {
+        if (this.invaders.Count<=0)
+        {
+            return;
+        }
+        var lastToRemove = this.invaders[count - 1];
+        var toRemove = this.invaders.RangeTo(lastToRemove, true).ToArray();
+
+        this.invaders.RemoveMany(toRemove);
+    }
+
+    public void DestroyTargetsInRadius(int radius)
+    {
+        this.invaders.RemoveAll(x => x.Distance <= radius);
+    }
+
+    public IEnumerable<Invader> Invaders()
+    {
+        foreach (var invader in this.invaders)
+        {
+            yield return (Invader)invader;
+        }
+    }
+}
---
+++ Exam09092017/01.Invaders/Invaders/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Computer computer = new Computer(100);
+        List<Invader> expected = new List<Invader>();
+
+        for (int i = 1; i <= 20000; i++)
+        {
+            var invader = new Invader(1, i);
+            computer.AddInvader(invader);
+            expected.Add(invader);
+        }
+
+        expected = expected.OrderBy(x => x.Distance).ThenBy(x => -x.Damage).Skip(10000).ToList();
+
+        computer.DestroyHighestPriorityTargets(10000);
+
+
+        var actual = computer.Invaders().ToList();
+        ;
+    }
+}
